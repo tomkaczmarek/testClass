f15ebee12e19dc68a7b63ecb93dfd9d2eca4d412
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-01-13 11:09:19 +0100

    class to branch

---
+++ TestCode/ClassToBranch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCode
+{
+    public class ClassToBranch
+    {
+        public string HelloBranch()
+        {
+            return "Hello to branch";
+        }
+    }
+}
