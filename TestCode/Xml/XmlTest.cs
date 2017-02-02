252685273ae39d122470e436724e43f47e9a635f
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-02 08:56:13 +0100

    Xml part2

--- TestCode/Xml/XmlTest.cs
+++ TestCode/Xml/XmlTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Linq;
 
 namespace TestCode.Xml
@@ -11,8 +12,47 @@ namespace TestCode.Xml
             var rootElem = new XElement("Customers");
             doc.Add(rootElem);
 
+            var customers = CreateCustomer(10);
+
+            foreach(Customer c in customers)
+            {
+                var customer = new XElement("Customer");
+                var firstNameElement = new XElement("FirstName", c.Name);
+                customer.Add(firstNameElement);
+
+                var ageElement = new XElement("Age", c.Age);
+                customer.Add(ageElement);
+
+                rootElem.Add(customer);
+            }
+
 
             Console.WriteLine(doc.ToString());
         }
+
+        private IList<Customer> CreateCustomer(int count)
+        {
+            IList<Customer> list = new List<Customer>();
+            string[] names = new string[] { "Tomek", "Jacek", "Marcin", "Bolek", "Jarek" };           
+            Random r = new Random();
+            
+            for(int i = 0; i<count; i++)
+            {
+                Customer c = new Customer()
+                {
+                    Age = r.Next(25, 50),
+                    Name =names[r.Next(0,4)]
+                };
+                list.Add(c);
+            }
+
+            return list;
+        }
+    }
+
+    class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
