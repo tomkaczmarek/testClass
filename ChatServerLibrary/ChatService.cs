bbee65531785a9506e9eb738d39f066849798759
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-02 11:52:11 +0100

    WCF Test

---
+++ ChatServerLibrary/ChatService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatServerLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChatService" in both code and config file together.
+    public class ChatService : IChatService
+    {
+        public void PostNote(string from, string note)
+        {
+            
+        }
+    }
+}
---
+++ ChatServerLibrary/IChatService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatServerLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IChatService" in both code and config file together.
+    [ServiceContract]
+    public interface IChatService
+    {
+        [OperationContract]
+        void PostNote(string from, string note);
+    }
+}
