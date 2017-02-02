a5f8f5534db397c5621f2e8abe4f06eefd1de70e
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-02-02 10:57:14 +0100

    Xml part3 serialization desearialization

--- TestCode/Xml/XmlTest.cs
+++ TestCode/Xml/XmlTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml.Linq;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace TestCode.Xml
 {
@@ -14,20 +17,50 @@ namespace TestCode.Xml
 
             var customers = CreateCustomer(10);
 
-            foreach(Customer c in customers)
+            //foreach(Customer c in customers)
+            //{
+            //    //first method
+            //    //var customer = new XElement("Customer");
+            //    //var firstNameElement = new XElement("FirstName", c.Name);
+            //    //customer.Add(firstNameElement);
+
+            //    //var ageElement = new XElement("Age", c.Age);
+            //    //customer.Add(ageElement);
+
+            //    //rootElem.Add(customer);
+
+
+            //    //second method
+            //    //customer = new XElement("Customer", new XElement("FirstName", c.Name, new XElement("SecondName", c.Name)), new XElement("Age", c.Age));
+
+            //    //rootElem.Add(customer);               
+
+            //}
+
+            //Console.WriteLine(doc.ToString());
+
+            //third method
+            //var customerXml = new XDocument(new XElement(
+            //    "Customers", from customer in customers
+            //                 select new XElement("Customer", new XAttribute("FirstName", customer.Name),
+            //                 new XAttribute("Age", customer.Age))));
+
+            //Console.WriteLine(customerXml);
+
+            //fourth method serialization and deserialization
+            customers = CreateCustomer(15);
+
+            var c = new Customer() { Name = "Tomek", Age = 15 };
+            Customer c1;
+            XmlSerializer serializer = new XmlSerializer(typeof(Customer));
+            using (StringWriter writer = new StringWriter())
             {
-                var customer = new XElement("Customer");
-                var firstNameElement = new XElement("FirstName", c.Name);
-                customer.Add(firstNameElement);
-
-                var ageElement = new XElement("Age", c.Age);
-                customer.Add(ageElement);
-
-                rootElem.Add(customer);
+                serializer.Serialize(writer, c);
+                Console.WriteLine(writer.ToString());
+                c1 = serializer.Deserialize(new StringReader(writer.ToString())) as Customer;
             }
 
-
-            Console.WriteLine(doc.ToString());
+            Console.WriteLine(c1.Name);      
         }
 
         private IList<Customer> CreateCustomer(int count)
@@ -50,9 +83,11 @@ namespace TestCode.Xml
         }
     }
 
-    class Customer
+    public class Customer
     {
+        [XmlAttribute]
         public string Name { get; set; }
+
         public int Age { get; set; }
     }
 }
