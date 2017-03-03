f451f07f48735574d2eeb775255a0179ba1e4fcf
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-03-03 10:41:44 +0100

    attribute

---
+++ TestCode/Attributes/TestAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCode.Attributes
+{
+    [AttributeUsage(AttributeTargets.Constructor,AllowMultiple =true)]
+    public class TestAttribute : Attribute
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Comment { get; set; }
+
+        public TestAttribute(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        
+    }
+}
