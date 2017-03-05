126bd439f8fb6172751c97e5f7100f798bf61749
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-03-05 14:32:22 +0100

    threads

---
+++ Threading/ThreadTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Threading
+{
+    public class ThreadTest
+    {
+        public async Task MyMethosAsync()
+        {
+            Task<int> longRunningTask = LongRunningOperation();
+
+            int result = await longRunningTask;
+
+            Console.WriteLine(result);
+        }
+
+        private async Task<int> LongRunningOperation()
+        {
+            await Task.Delay(2000);
+            return 1;
+        }
+    }
+}
