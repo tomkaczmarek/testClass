4bb1751531f33232697f24059d30c45732a6fcce
Tomasz Kaczmarek <t.kaczmarek@wp.pl>
2017-03-22 13:35:51 +0100

    some stuff from c#7

---
+++ TestCode/CSharp7/CSharp7Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCode.CSharp7
+{
+    class CSharp7Class
+    {
+        private void TestMethod()
+        {
+            //local function
+            Console.WriteLine(Fib(1));
+
+            int Fib(int n) => n < 2 ? n : Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
